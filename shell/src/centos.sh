7b2d3968ed2762ab6d6e688c565fcf05184331df
hqukai@gmail.com <hankai123>
2016-01-09 10:55:54 +0800

    xxxxxxxx

---
+++ shell/src/centos.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+
+
+#//查看安装的程序
+rpm –qa|grep -i mysql
+#卸载
+rqm -e mysql**
+
--- sql/src/oracle.sql
+++ sql/src/oracle.sql
@@ -5,3 +5,17 @@ insert into mer_certificate t (t.id,t.cer_seq,t.cer_start,cer_over,t.cer_type,t.
   (select 4 as id, cer_seq,cer_start,cer_over,cer_type,cer_status,create_time, '000001110100000002' as mer_id,cer_data
 from mer_certificate t where id = 2);
 commit;
+
+
+--查找会话
+SELECT object_name, machine, s.sid, s.serial#
+FROM gv$locked_object l, dba_objects o, gv$session s
+WHERE l.object_id　= o.object_id
+AND l.session_id = s.sid;
+--kill 会话
+ALTER system kill session '52, 11825';
+
+
+--添加字段
+alter table pp_order add(remark varchar(200));
+COMMENT ON COLUMN PP_ORDER.remark IS '自定义保留域';
